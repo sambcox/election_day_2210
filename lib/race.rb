bcaadcc6d3b7c57017d9d0596b944b4335095e71
Samuel Cox <samc1253@gmail.com>
2022-11-01 09:56:59 -0600

    Allow open to return true in election

--- lib/race.rb
+++ lib/race.rb
@@ -3,11 +3,16 @@ class Race
   def initialize(office)
     @office = office
     @candidates = []
+    @open = true
   end
 
   def register_candidate!(data)
     new_candidate = Candidate.new(data)
     @candidates << new_candidate
     new_candidate
+  end
+
+  def open?
+    @open
   end
 end
