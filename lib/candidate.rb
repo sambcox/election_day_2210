482d38847109ffacb875c7bf0c8bbcda3ac04c4f
Samuel Cox <samc1253@gmail.com>
2022-11-01 09:11:28 -0600

    Add votes

--- lib/candidate.rb
+++ lib/candidate.rb
@@ -1,7 +1,8 @@
 class Candidate
-  attr_reader :name, :party
+  attr_reader :name, :party, :votes
   def initialize(data)
     @name = data[:name]
     @party = data[:party]
+    @votes = 0
   end
 end
