020068e177a8794d7eda2e468e4b8823d3ef2962
Samuel Cox <samc1253@gmail.com>
2022-11-01 10:23:51 -0600

    Add winners test for election class

--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -65,4 +65,24 @@ RSpec.describe Election do
 
     expect(election.vote_counts).to eq({"Diana D"=>4, "Roberto R"=>1, "Diego D"=>10, "Rita R"=>6, "Ida I"=>6})
   end
+
+  it 'can return winners from all races in an election' do
+    election = Election.new('2022')
+    race1 = Race.new("Virginia District 4 Representative")
+    race2 = Race.new("Texas Governor")
+    election.add_race(race1)
+    election.add_race(race2)
+    candidate1 = race1.register_candidate!({name: "Diana D", party: :democrat})
+    candidate2 = race1.register_candidate!({name: "Roberto R", party: :republican})
+    candidate3 = race2.register_candidate!({name: "Diego D", party: :democrat})
+    candidate4 = race2.register_candidate!({name: "Rita R", party: :republican})
+    candidate5 = race2.register_candidate!({name: "Ida I", party: :independent})
+    4.times {candidate1.vote_for!}
+    1.times {candidate2.vote_for!}
+    10.times {candidate3.vote_for!}
+    6.times {candidate4.vote_for!}
+    6.times {candidate5.vote_for!}
+
+    expect(election.winners).to eq([candidate1, candidate3])
+  end
 end
