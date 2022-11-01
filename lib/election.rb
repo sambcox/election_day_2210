369f080fc8bb991c8a9ab7e407462247da5626d8
Samuel Cox <samc1253@gmail.com>
2022-11-01 09:49:00 -0600

    Add method to return total vote counts

--- lib/election.rb
+++ lib/election.rb
@@ -8,4 +8,16 @@ class Election
   def add_race(race)
     @races << race
   end
+
+  def candidates
+    @races.map { |race| race.candidates}.flatten
+  end
+
+  def vote_counts
+    overall_count = {}
+    candidates.each do |candidate|
+      overall_count[candidate.name] = candidate.votes
+    end
+    overall_count
+  end
 end
